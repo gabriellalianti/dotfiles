769f020ea603b44e9ebd40f6ad6ee268eaaf51fd
Gabriella L <134288268+gabriellalianti@users.noreply.github.com>
2025-11-12 22:56:21 +1100

    add delta, bat, fzf, thefuck configs

--- zsh/zsh-public.zsh
+++ zsh/zsh-public.zsh
@@ -53,6 +53,44 @@ eval "$(starship init zsh)"
 source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
+# thefuck (cmd correction lol)
+eval $(thefuck --alias shibal)
+
+# fuzzy find (ctrl+T) 
+# ssh **, cd **
+# ctrl+R for history
+eval "$(fzf --zsh)"
+# use fd instead of fzf
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+# use fd (https://github.com/sharkdp/fd) for listing path candidates.
+# - the first argument to the function ($1) is the base path to start traversal
+# - see the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --exclude .git . "$1"
+}
+# use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type=d --hidden --exclude .git . "$1"
+}
+# fzf with git
+source ~/fzf-git.sh/fzf-git.sh
+# previews with eza and bat from josean martinez on yt : )
+show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
+export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+_fzf_comprun() {
+  local command=$1
+  shift
+  case "$command" in
+    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export|unset) fzf --preview "eval 'echo \${}'"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
+  esac
+}
+
 # copy wezterm config from wsl to windows
 alias sync-wezterm="cp ~/.dotfiles/wezterm/.wezterm.lua /mnt/c/Users/Gabriella/.wezterm.lua"
 
