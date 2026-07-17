9d1b3af5c9b12ea57497e3b54d32ad3f843f7e3f
Gabriella Lianti <134288268+gabriellalianti@users.noreply.github.com>
2026-07-18 03:56:28 +1000

    add sketchybar bar option, moved items to files

--- sketchybar/.config/sketchybar/items/volume.sh
+++ sketchybar/.config/sketchybar/items/volume.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sketchybar --add item volume right \
-           --set volume script="$PLUGIN_DIR/volume.sh" \
+           --set volume \
+                 label.padding_right=10 \
+                 script="$PLUGIN_DIR/volume.sh" \
            --subscribe volume volume_change
