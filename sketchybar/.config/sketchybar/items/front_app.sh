6d45074accaeaa5464ef4d6039ba9c9fc871d1b1
Gabriella Lianti <134288268+gabriellalianti@users.noreply.github.com>
2026-07-18 18:23:32 +1000

    change appearance

--- sketchybar/.config/sketchybar/items/front_app.sh
+++ sketchybar/.config/sketchybar/items/front_app.sh
@@ -6,5 +6,8 @@ sketchybar --add item front_app left \
                  icon.color="$TEXT_COLOR" \
                  icon.font="sketchybar-app-font:Regular:12.0" \
                  label.color="$TEXT_COLOR" \
+                 background.color=$PURPLE \
+                 icon.color=$BLACK \
+                 label.color=$BLACK \
                  script="$PLUGIN_DIR/front_app.sh" \
            --subscribe front_app front_app_switched
