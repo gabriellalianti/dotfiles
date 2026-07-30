787acab7a8ecaaad422f5f5d60ac0653c0a1d89c
Gabriella Lianti <134288268+gabriellalianti@users.noreply.github.com>
2026-07-30 14:38:55 +1000

    feat(yazi): add initial config and themes

--- zsh/zsh-public.zsh
+++ zsh/zsh-public.zsh
@@ -126,6 +126,15 @@ _fzf_comprun() {
   esac
 }
 
+# yazi shell wrapper
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	command rm -f -- "$tmp"
+}
+
 # history setup
 HISTFILE=$HOME/.zhistory
 SAVEHIST=1000
